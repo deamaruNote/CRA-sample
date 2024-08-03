fa8c5c94d96ee0c4aad650962e17b837b6b31e07
Maru <3q.cindy@gmail.com>
2024-08-03 10:42:11 +0800

    Updates

---
+++ precache-manifest.c9f4414e101ed6a9e7e2b0c3d3ea75a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e00f5fe1f166ded98b464bfc4391e2",
+    "url": "/CRA-sample/index.html"
+  },
+  {
+    "revision": "796e4331e9cd90e9c589",
+    "url": "/CRA-sample/static/css/main.71fbfa11.chunk.css"
+  },
+  {
+    "revision": "cb94078f0ed4cf8bf980",
+    "url": "/CRA-sample/static/js/2.990dd9f0.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/CRA-sample/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "796e4331e9cd90e9c589",
+    "url": "/CRA-sample/static/js/main.3b86e16b.chunk.js"
+  },
+  {
+    "revision": "01629ba0f40eaf708dcd",
+    "url": "/CRA-sample/static/js/runtime~main.998bd000.js"
+  }
+]);
